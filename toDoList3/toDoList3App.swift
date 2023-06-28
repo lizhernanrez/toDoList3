556e8b92ec365ecc57bd1eee5c46b027ae9bf224
Liz Hernandez <lizcarol2005@gmail.com>
2023-06-28 10:47:40 -0400

    Initial Commit

---
+++ toDoList3/toDoList3App.swift
@@ -0,0 +1,17 @@
+//
+//  toDoList3App.swift
+//  toDoList3
+//
+//  Created by Tomas Hernandez on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct toDoList3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
